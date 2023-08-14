226ce13205af694690a763413f61e8ebbeb65a70
Abhishek kumar <abhi.arya97161@gmail.com>
2023-08-14 09:14:07 +0530

    footer completed

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ErrorPage from './ErrorPage';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -53,6 +54,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='*' element={<ErrorPage />} />
         </Routes>
+        <Footer />
       </Router>
     </ThemeProvider>
   );
