92b11795729b5bb94f33b62eb6592386ac00a1b6
Abhishek kumar <abhi.arya97161@gmail.com>
2023-08-14 16:20:33 +0530

    products get through axios

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,8 +1,21 @@
-import  { createContext, useContext } from 'react';
+import  { createContext, useContext, useEffect } from 'react';
+import axios from 'axios';
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+
+    const API = "https://api.pujakaitem.com/api/products";
+
+    const getProducts = async (url) => {
+        const res = await axios.get(url);
+        const products =  await res.data;
+    }
+
+    useEffect(()=>{
+        getProducts(API);
+    },[])
+
     return <AppContext.Provider value={{myName:"vinod",surName:"kumar"}}>{ children }</AppContext.Provider>
 };
 
