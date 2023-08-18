cdc19577e5c8b3bf5f66cda95623c0dfd901fd76
Abhishek kumar <abhi.arya97161@gmail.com>
2023-08-18 21:53:15 +0530

    color picker added

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -9,6 +9,7 @@ import PageNavigation from "./components/PageNavigation";
 import { MdSecurity } from "react-icons/md";
 import { TbTruckDelivery, TbReplace } from "react-icons/tb";
 import Star from "./components/Star";
+import AddToCart from './components/AddToCart';
 
 const API = "https://api.pujakaitem.com/api/products";
 
@@ -53,8 +54,8 @@ const SingleProduct = () => {
           {/* product dAta  */}
           <div className="product-data">
             <h2>{name}</h2>
-            <Star stars={stars} reviews ={reviews} />
-           
+            <Star stars={stars} reviews={reviews} />
+
             <p className="product-data-price">
               MRP:
               <del>
@@ -99,6 +100,8 @@ const SingleProduct = () => {
                 Brand :<span> {company} </span>
               </p>
             </div>
+            <hr />
+            {stock > 0 && <AddToCart product={singleProduct} />}
           </div>
         </div>
       </Container>
