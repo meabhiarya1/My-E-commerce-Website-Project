c4d2f34481e90104f4efe27f7bf1d2d5f2e48e26
Abhishek kumar <abhi.arya97161@gmail.com>
2023-09-01 10:29:49 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
+  // we just have to change domain and clientId  if expires from Auth0
+
   <Auth0Provider
     domain="dev-u2nom7kvfh71pm2s.us.auth0.com"
     clientId="MQBnTplkHjJcbRQn8iMaLMkoD1oqtJnd"
